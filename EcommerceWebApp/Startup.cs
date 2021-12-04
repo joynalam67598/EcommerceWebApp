317466e94cd8d9d5667d2cf4c8d68397291a85ae
joynal67598 <59138477+joynalam67598@users.noreply.github.com>
2021-12-05 01:42:00 +0600

    worked on header

--- EcommerceWebApp/Startup.cs
+++ EcommerceWebApp/Startup.cs
@@ -24,6 +24,7 @@ namespace EcommerceWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
 
         }
 
