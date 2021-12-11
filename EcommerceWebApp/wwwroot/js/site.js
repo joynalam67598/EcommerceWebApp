df85be07f024bfb1d5d3dda94d659ae385994693
joynal67598 <59138477+joynalam67598@users.noreply.github.com>
2021-12-11 11:52:26 +0600

    add owl slider

--- EcommerceWebApp/wwwroot/js/site.js
+++ EcommerceWebApp/wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$('.owl-carousel').owlCarousel({
+        autoplay: true,
+        autoplayhoverpause: true,
+        autoplaytimeout: 1000,
+        loop: true,
+        nav: true,
+        responsive: {
+            480: { items: 2 }, // from zero to 480 screen width 4 items
+            768: { items: 4 }, // from 480 screen widthto 768 6 items
+            1024: {
+                items: 6   // from 768 screen width to 1024 8 items
+            }
+        },
+    });
